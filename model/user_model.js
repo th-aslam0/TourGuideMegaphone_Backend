a2b6a4239ab6aa234e15a39f58c16dcaf6db216c
Tamoor Haider <tamoor.h.aslam@gmail.com>
2024-03-24 15:08:39 -0700

    [BE]: added missing fields

--- model/user_model.js
+++ model/user_model.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const Schema =mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    fname:String,
+    lname:String
 })
 const User=mongoose.model('User',Schema);
 module.exports= User
